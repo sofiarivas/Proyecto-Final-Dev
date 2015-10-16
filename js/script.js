5bad4484ddd542b241b33c295eed195651907c15
Sofía Rivas <sofi.rivasm@gmail.com>
2015-10-16 01:47:33 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//$(document).foundation();
+
+$(document).foundation('section').foundation('orbit', {
+    animation: 'fade',
+    timer_speed: 3000,
+    animation_speed: 400,
+    stack_on_small: false,
+    navigation_arrows: false,
+    slide_number: false,
+    pause_on_hover: false,
+    resume_on_mouseout: false,
+    bullets: false,
+    timer: true,
+    variable_height: false,
+  });
